bb8b11d1c05786948796eed04a050ead72616a09
TakeKiyo <d8sk4ueum@gmail.com>
2021-02-22 20:05:20 +0900

    gurobi使えることを確認

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 a.out : main.o InputData.o
-	g++ main.o InputData.o
+	g++ main.o InputData.o -I/Library/gurobi900/mac64/include -L/Library/gurobi900/mac64/lib -lgurobi_c++ -lgurobi90
 main.o : main.cpp InputData.hpp
-	g++ -c main.cpp -o main.o
+	g++ -c main.cpp -o main.o -I/Library/gurobi900/mac64/include -L/Library/gurobi900/mac64/lib -lgurobi_c++ -lgurobi90
 InputData.o : InputData.cpp InputData.hpp 
 	g++ -c InputData.cpp -o InputData.o
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "InputData.hpp"
+#include <gurobi_c++.h>
 
 using namespace std;
 
@@ -9,4 +10,16 @@ int main()
     cout << "test" << endl;
     InputData inputData;
     inputData.Read_booking();
+    try
+    {
+
+        GRBEnv env = GRBEnv(true);
+        env.set("LogFile", "mip1.log");
+        env.start();
+    }
+    catch (GRBException e)
+    {
+        cout << "Error during optimization" << endl;
+        cout << e.getMessage() << endl;
+    }
 }
