8504d8df6626ae01449c0cecd7ef945430350c49
TakeKiyo <d8sk4ueum@gmail.com>
2021-06-01 15:31:56 +0900

    セグメントと，それに対応した面積の配列を作った

---
+++ heuristic.py
@@ -0,0 +1,72 @@
+import warnings
+import numpy as np
+import pandas as pd
+import gurobipy as gp
+import category_encoders as ce
+from sklearn.preprocessing import LabelEncoder
+import read_booking
+import read_hold
+import read_other
+
+import sys
+args = sys.argv
+
+warnings.filterwarnings("ignore")
+
+
+def main():
+    BookingFile = "book/exp.csv"
+    # BookingFile = args[1]
+    HoldFile = "data/hold.csv"
+    MainLampFile = "data/mainlamp.csv"
+    BackMainLampFile = "data/back_mainlamp.csv"
+    AfrMainLampFile = "data/afr_mainlamp.csv"
+    StressFile = "data/stress_mainlamp.csv"
+    Gang2File = "data/gangnum_2.csv"
+    Gang3File = "data/gangnum_3.csv"
+
+    # print("File:" + BookingFile)
+    booking_name = BookingFile.split("/")[1].split(".")[0]
+    # 注文情報の読み込み
+    T, L, D, J, U, A, G, J_small, J_medium, J_large, Port, Check_port, Booking, divide_dic\
+        = read_booking.Read_booking(BookingFile)
+
+    # 船体情報の読み込み1
+    I, B, I_pair, I_next, I_same, I_lamp, I_deck, RT_benefit, delta_s, min_s, max_s, delta_h, max_h, Hold_encode, Hold\
+        = read_hold.Read_hold(HoldFile)
+
+    # 船体情報の読み込み2
+    Ml_Load, Ml_Back, Ml_Afr, Stress, GANG2, GANG3\
+        = read_other.Read_other(MainLampFile, BackMainLampFile, AfrMainLampFile, StressFile, Gang2File, Gang3File, Hold_encode)
+
+    segments = np.array([[1, 0],
+                         [2, 3],
+                         [4],
+                         [5, 6, 7],
+                         [8, 9, 10],
+                         [11, 12, 13, 14],
+                         [18, 17, 16, 15],
+                         [22, 21, 20, 19],
+                         [23, 24],
+                         [26, 25],
+                         [27, 28],
+                         [30, 29],
+                         [31, 32],
+                         [34, 33],
+                         [35, 36],
+                         [38, 37],
+                         [39, 40],
+                         [42, 41]]
+                        )
+    # 分割したホールドで，奥から詰める順番で配列を作成
+    each_segments_size = []
+    for i in range(len(segments)):
+        each_segments_size.append([])
+        for j in range(len(segments[i])):
+            each_segments_size[i].append(B[segments[i][j]])
+
+    print(each_segments_size)
+
+
+if __name__ == "__main__":
+    main()
