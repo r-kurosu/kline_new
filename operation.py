2b250c7b79d5b515a84502b5f41bf3614f1497eb
TakeKiyo <d8sk4ueum@gmail.com>
2021-06-02 15:14:22 +0900

    シフト近傍のコード書いた

---
+++ operation.py
@@ -0,0 +1,27 @@
+import random
+random.seed(1)
+
+def find_loading_port(order_num,J_t_load):
+    for port_num in range(len(J_t_load)):
+        if order_num in J_t_load[port_num]:
+            return port_num
+        
+def shift(assignment,order_num,next_segment_num,loading_port):
+    current_segment_num = -1
+    #どのセグメントに入っているか確認
+    for seg in range(len(assignment)):
+        if current_segment_num != -1:
+            break
+        if order_num in assignment[seg][loading_port]:
+            current_segment_num = seg
+    
+    #挿入先が変わらないならreturn
+    if current_segment_num == next_segment_num:
+        return assignment
+    
+    # いまの割当から削除
+    assignment[current_segment_num][loading_port].remove(order_num)
+    #新しく挿入
+    random_order = random.randint(0,len(assignment[next_segment_num][loading_port]))
+    assignment[next_segment_num][loading_port].insert(random_order,order_num)
+    return assignment
