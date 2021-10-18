21ccd52ec5771a78a7ce018d9bf09311ebface3b
TakeKiyo <d8sk4ueum@gmail.com>
2021-10-18 17:30:51 +0900

    バッチファイル書いてみた

---
+++ batch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+/usr/bin/python3 /home/takeda/KLINE/hybrid.py book/exp.csv > log_hybrid1018/exp_latest.out
+/usr/bin/python3 /home/takeda/KLINE/hybrid.py book/exp_2_5.csv > log_hybrid1018/exp_2_5.out
+/usr/bin/python3 /home/takeda/KLINE/hybrid.py book/exp_4_3.csv > log_hybrid1018/exp_4_3.out
+
